62d6ae6a1a29c62341dd9ded354386028d57c974
Mikaël Léger <mikael.leger8@gmail.com>
2022-11-16 13:39:51 +0100

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders input text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputElement = document.getElementsByClassName("task")[0];
+  expect(inputElement).toBeInTheDocument();
 });
